69c47ec8969ff1deeab4d24d0db958af26a76b25
Franck W <franck.rj.w@gmail.com>
2017-10-06 10:58:57 +0200

    Possible correction d'un bug empêchant la récupération des notif'.

---
+++ kotlin/com/franckrj/jvnotif/InitThingsBootAndUpgradeReceiver.kt
@@ -0,0 +1,21 @@
+package com.franckrj.jvnotif
+
+import android.content.Intent
+import android.content.BroadcastReceiver
+import android.content.Context
+import com.franckrj.jvnotif.utils.InitShedulesManager
+import com.franckrj.jvnotif.utils.NotifsManager
+
+class InitThingsBootAndUpgradeReceiver : BroadcastReceiver() {
+    /*A ce qu'il parait ces 3 premiers intents sont nécessaire pour être sur que ça marche.*/
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == "android.intent.action.BOOT_COMPLETED" ||
+            intent.action == "android.intent.action.QUICKBOOT_POWERON" ||
+            intent.action == "com.htc.intent.action.QUICKBOOT_POWERON" ||
+            intent.action == "android.intent.action.MY_PACKAGE_REPLACED") {
+            NotificationDismissedReceiver.onNotifDismissed(NotifsManager.MP_NOTIF_ID)
+
+            InitShedulesManager.initSchedulers(context)
+        }
+    }
+}
