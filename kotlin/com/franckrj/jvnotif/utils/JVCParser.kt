bfd50506d742665dce6927534339742cdec8a02c
FranckRJ <franck.rj.w@gmail.com>
2018-02-21 11:24:07 +0100

    Correction d'un bug de récupération des notifications.

--- kotlin/com/franckrj/jvnotif/utils/JVCParser.kt
+++ kotlin/com/franckrj/jvnotif/utils/JVCParser.kt
@@ -4,8 +4,8 @@ import java.util.regex.Matcher
 import java.util.regex.Pattern
 
 object JVCParser {
-    private val numberOfMpJVCPattern = Pattern.compile("""<div class=".*?account-mp.*?">[^<]*<span[^c]*class="account-number-mp[^"]*".*?data-val="([^"]*)"""", Pattern.DOTALL)
-    private val numberOfStarsJVCPattern = Pattern.compile("<div class=\".*?account-notif.*?\">[^<]*<span[^c]*class=\"account-number-notif[^\"]*\".*?data-val=\"([^\"]*)\"", Pattern.DOTALL)
+    private val numberOfMpJVCPattern = Pattern.compile("""<div class=".*?account-mp.*?">[^<]*<span[^c]*class="jv-account-number-mp[^"]*".*?data-val="([^"]*)"""", Pattern.DOTALL)
+    private val numberOfStarsJVCPattern = Pattern.compile("<div class=\".*?account-notif.*?\">[^<]*<span[^c]*class=\"jv-account-number-notif[^\"]*\".*?data-val=\"([^\"]*)\"", Pattern.DOTALL)
 
     fun getNumberOfMpFromPage(pageSource: String): Int {
         val numberOfMpJVCMatcher: Matcher = numberOfMpJVCPattern.matcher(pageSource)
@@ -19,7 +19,7 @@ object JVCParser {
     }
 
     fun getNumberOfStarsFromPage(pageSource: String): Int {
-        val numberOfStarsJVCMatcher: Matcher = numberOfStarsJVCPattern.matcher(pageSource);
+        val numberOfStarsJVCMatcher: Matcher = numberOfStarsJVCPattern.matcher(pageSource)
 
         @Suppress("LiftReturnOrAssignment")
         if (numberOfStarsJVCMatcher.find()) {
